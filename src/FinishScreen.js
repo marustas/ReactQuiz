38446fdcc2d2c212e93d05dc8f275ce729a1397c
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-05 17:43:58 +0100

    Result data in the finish screen

--- src/FinishScreen.js
+++ src/FinishScreen.js
@@ -1,8 +1,27 @@
 const FinishScreen = ({ points, maxPoints }) => {
   const percentage = Math.ceil((100 * points) / maxPoints);
+  let emoji;
+  switch (percentage) {
+    case 100:
+      emoji = "🥇";
+      break;
+    case percentage >= 80 && percentage < 100:
+      emoji = "🎉";
+      break;
+    case percentage >= 50 && percentage < 80:
+      emoji = "🙂";
+      break;
+    case percentage > 0 && percentage < 50:
+      emoji = "🤔";
+      break;
+    default:
+      emoji = "🤦‍♂️";
+      break;
+  }
   return (
     <p className="result">
-      You scored <strong>{points}</strong> out of {maxPoints} ({percentage} %)
+      {emoji} You scored <strong>{points}</strong> out of {maxPoints} (
+      {percentage} %)
     </p>
   );
 };
