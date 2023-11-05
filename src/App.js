f6955098b900885cf596d1ac7a57e71b474ca26c
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-05 14:33:26 +0100

    Added a dummy server for fetching

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+function reducer(state, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case "dataReceived":
+      return { ...state, question: payload, status: "ready" };
+
+    default:
+      throw new Error("Unknown type");
+  }
+}
 function App() {
+  const [state, dispath] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((response) => response.json())
+      .then((data) => dispath({ type: "dataReceived", payload: data }))
+      .catch((error) => console.log("Error"));
+  }, []);
   return (
     <div className="app">
       <Header />
