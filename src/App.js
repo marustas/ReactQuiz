b6400de7dcf26c882ff5184dce2edd65fd084f96
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-05 14:53:00 +0100

    Polished the starter screen

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ const initialState = {
   questions: [],
   status: "loading",
 };
+
 function reducer(state, action) {
   const { type, payload } = action;
   switch (type) {
     case "dataReceived":
-      return { ...state, question: payload, status: "ready" };
+      return { ...state, questions: payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
 
@@ -21,21 +22,24 @@ function reducer(state, action) {
       throw new Error("Unknown type");
   }
 }
+
 function App() {
   const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((response) => response.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((error) => dispatch({ type: "dataFailed" }));
   }, []);
+
   return (
     <div className="app">
       <Header />
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen />}
+        {status === "ready" && <StartScreen numQuestions={questions.length} />}
       </Main>
     </div>
   );
--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,9 +1,9 @@
-const StartScreen = () => {
+const StartScreen = ({ numQuestions }) => {
   return (
     <div className="start">
       <h2>Welcome to the React Quiz</h2>
-      <h3>X questions to test your React Mastery</h3>
-      <button>Let's start</button>
+      <h3>{numQuestions} questions to test your React Mastery</h3>
+      <button className="btn btn-ui">Let's start</button>
     </div>
   );
 };
