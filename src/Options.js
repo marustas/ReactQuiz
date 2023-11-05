284ca9674bba016210ef55b91b039a8232a7ff23
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-05 15:08:52 +0100

    Question and options split into 2 components

---
+++ src/Options.js
@@ -0,0 +1,13 @@
+const Options = ({ options }) => {
+  return (
+    <div className="options">
+      {options.map((option) => (
+        <button key={option} className="btn btn-option">
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Options;
--- src/Question.js
+++ src/Question.js
@@ -1,15 +1,10 @@
+import Options from "./Options";
 const Question = ({ question }) => {
   const { options } = question;
   return (
     <div>
       <h4> {question.question}</h4>
-      <div className="options">
-        {options.map((option) => (
-          <button key={option} className="btn btn-option">
-            {option}
-          </button>
-        ))}
-      </div>
+      <Options options={options} />
     </div>
   );
 };
